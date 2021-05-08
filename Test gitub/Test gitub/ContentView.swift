a86f473387817d30dfcbff64cd4fd278c11a9505
Charles-Edouard Girard <cegirard@gmail.com>
2021-05-08 08:37:32 +0200

    premier commit

---
+++ Test gitub/Test gitub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test gitub
+//
+//  Created by GIRARD CHARLES-EDOUARD on 08/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("coucou")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test gitub/Test gitub/Test_gitubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_gitubApp.swift
+//  Test gitub
+//
+//  Created by GIRARD CHARLES-EDOUARD on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Test_gitubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
